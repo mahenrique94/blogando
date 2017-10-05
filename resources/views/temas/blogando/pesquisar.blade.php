ff3bbbd99427fc5a8384a51117e64cf6384419dc
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2017-10-05 16:08:21 -0300

    Acerto no placeholder do input de pesquisa para blogs

--- resources/views/temas/blogando/pesquisar.blade.php
+++ resources/views/temas/blogando/pesquisar.blade.php
@@ -2,7 +2,7 @@
     <h3 class="bg-aside__titulo">@lang("messages.caixa.pesquisar")</h3>
     <form action="/procurar" class="bg-pesquisar o-form" id="formpesquisarpost" method="get" name="formpesquisarpost">
         <div class="o-form__group">
-            <input aria-required="true" class="o-form__data bg-pesquisar__data" id="filtro" name="filtro" placeholder="Procurar por um post" required type="search" value="{{isset($filtro) && !is_null($filtro) && !empty($filtro) ? $filtro : null}}">
+            <input aria-required="true" class="o-form__data bg-pesquisar__data" id="filtro" name="filtro" placeholder="Digite aqui o título ou uma parte dele" required type="search" value="{{isset($filtro) && !is_null($filtro) && !empty($filtro) ? $filtro : null}}">
             <span class="o-form__groupElement"><button class="o-button--gary"><i class="icon-search"></i></button></span>
         </div>
     </form>
