e1f37d78ce380cd0260024a85b6a642bf606cf79
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-01-23 21:42:40 -0200

    Adicionado powerer by do Digital Ocean no tema do MHC

---
+++ resources/views/temas/mhc/footer.blade.php
@@ -0,0 +1,24 @@
+@section("footer")
+    <ul class="bg-footer__lista">
+        <li class="bg-footer__item"><a class="bg-footer__link bg-footer__link--titulo" href="#">Redes Sociais</a></li>
+        @foreach ($blogredessociais as $redesocial)
+            <li class="bg-footer__item"><a class="bg-footer__link" href="{{$redesocial->link}}" target="_blank">{{$redesocial->redesocial->descricao}}</a></li>
+        @endforeach
+    </ul>
+    <ul class="bg-footer__lista">
+        <li class="bg-footer__item"><a class="bg-footer__link bg-footer__link--titulo" href="#">Categorias</a></li>
+        @foreach ($categorias as $categoria)
+            <li class="bg-footer__item"><a class="bg-footer__link" href="/categoria/{{$categoria->slug}}">{{$categoria->descricao}}</a></li>
+        @endforeach
+    </ul>
+    <ul class="bg-footer__lista">
+        <li class="bg-footer__item"><a class="bg-footer__link bg-footer__link--titulo" href="#">Posts Recentes</a></li>
+        @foreach ($postsrecentes as $post)
+            <li class="bg-footer__item"><a class="bg-footer__link" href="/{{$post->slug}}">{{$post->titulo}}</a></li>
+        @endforeach
+    </ul>
+    @include("temas.blogando.newsletter")
+    <div class="bg-footer__sponsorship">
+        <a href="https://m.do.co/c/f34f73a7da3e" target="_blank"><img alt="Powered by Digital Ocean" src="/assets/temas/mhc/sponsorship/do-powered-by-badge-blue.svg"></a>
+    </div>
+@stop
--- resources/views/temas/mhc/visualizar.blade.php
+++ resources/views/temas/mhc/visualizar.blade.php
@@ -1,1 +1,42 @@
-@include("temas.blogando.visualizar")
+@extends("config.pagina")
+@section("titulo"){{$post->titulo}}@stop
+@include("temas.blogando.header")
+@section("conteudo")
+    <section class="bg-conteudo--visualizar u-content">
+        <section class="bg-visualizar">
+            <article class="bg-post">
+                @unless (Auth::guest())
+                    <a class="bg-post__editar o-button--tie o-button--medium" href="/painel/post/{{$post->id}}"><i class="icon-pencil"></i>@lang("messages.botao.editar")</a>
+                @endunless
+                <a href="/{{$post->slug}}"><div class="bg-post__imagem" style="background-image: url({{$blog->url}}/arquivo/download/posts/{{date_format(date_create($post->datapostagem), "Y")}}/{{date_format(date_create($post->datapostagem), "m")}}/{{$post->imagem}});"></div></a>
+                <header class="bg-post__cabecalho">
+                    <h2 class="bg-post__titulo"><a href="/{{$post->slug}}">{{$post->titulo}}</a></h2>
+                    <p class="bg-post__informacoes">
+                        Postado em <time class="bg-post__data">{{date_format(date_create($post->datapostagem), $blog->parametros->formatodatahora->formato)}}</time> por <a class="bg-post__autor" href="/autor/{{$post->perfil->slug}}">{{$post->perfil->nome}}</a>
+                    </p>
+                    <p class="bg-post__categorias">
+                        @foreach ($post->categorias as $categoria)
+                            <a class="bg-post__categoria bg-{{$categoria->categoria->slug}}" href="/categoria/{{$categoria->categoria->slug}}">{{$categoria->categoria->descricao}}</a>
+                        @endforeach
+                    </p>
+                </header>
+                <section class="bg-post__conteudo">{!!$post->conteudohtml!!}</section>
+                <footer class="bg-post__rodape">
+                    <p class="bg-post__tags">
+                        @foreach ($post->tags as $tag)
+                            <a class="bg-post__tag" href="/tag/{{$tag->tag->slug}}">{{$tag->tag->descricao}}</a>
+                        @endforeach
+                    </p>
+                </footer>
+            </article>
+        </section>
+        @include("temas.blogando.autor")
+        @if ($blog->parametros->permitircomentarios && $blog->parametros->permitircomentariosanonimos)
+            @include("temas.blogando.comentario")
+        @endif
+        @if ($blog->parametros->permitircomentarios && $blog->parametros->utilizarcomentariosdisqus)
+            @include("temas.blogando.disqus")
+        @endif
+    </section>
+@stop
+@include("temas.mhc.footer")
